24885ea2686034b244b4561d2882fd5676c49fc1
pigeongorl86 <maxine_liang@outlook.com>
2025-05-30 13:09:05 +1000

    May30 Changes

---
+++ class-changes.js
@@ -0,0 +1,14 @@
+const classChanges = [
+  { time: 7, target: ".mainpage", add: "active" },
+  { time: 21, target: ".mainpage", remove: "active" },
+
+  { time: 30, target: ".mainpage", add: "blur" },
+  { time: 34, target: ".mainpage", remove: "blur" },
+
+  { time: 16, target: ".changeme2", remove: "hidden" },
+  { time: 16, target: ".changeme2", add: "show" },
+
+  { time: 16, target: ".changeme2", remove: "hidden" },
+  { time: 28, target: ".changeme3", remove: "show" },
+  { time: 28, target: ".changeme3", add: "hidden" }
+];
