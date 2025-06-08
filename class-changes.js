4c1702539c8b2541606a19f02be0fa64cdda0440
pigeongorl86 <maxine_liang@outlook.com>
2025-06-08 18:57:04 +1000

    Jun 8 Changes

--- class-changes.js
+++ class-changes.js
@@ -2,38 +2,58 @@ const classChanges = [
   { time: 7, target: ".mainpage", add: "active" },
   { time: 21, target: ".mainpage", remove: "active" },
 
+  { time: 30, target: ".conciousness", remove: "smalltext" }, 
+  { time: 30, target: ".conciousness", remove: "width50" }, 
+  { time: 30, target: ".conciousness", add: "largetext" }, 
+  { time: 30, target: ".conciousness", add: "text-white" }, 
+
+  { time: 38, target: ".cloud1", add: "active"},
+  { time: 65, target: ".cloud1", remove: "active" },  
+
+  { time: 38, target: ".cloud2", add: "active" },
+  { time: 65, target: ".cloud2", remove: "active" }, 
+
   { time: 198, target: ".eye1", add: "active" },
   { time: 243, target: ".eye1", remove: "active" },
 
-  { time: 200, target: ".eye2", add: "active" },
+  { time: 199, target: ".eye2", add: "active" },
   { time: 243, target: ".eye2", remove: "active" },
 
-  { time: 202, target: ".eye3", add: "active" },
+  { time: 200, target: ".eye3", add: "active" },
   { time: 243, target: ".eye3", remove: "active" },
 
-  { time: 204, target: ".forehead", add: "active" },
+  { time: 201, target: ".forehead", add: "active" },
   { time: 243, target: ".forehead", remove: "active" },
 
-  { time: 206, target: ".eye4", add: "active" },
+  { time: 202, target: ".eye4", add: "active" },
   { time: 243, target: ".eye4", remove: "active" },
 
-  { time: 208, target: ".eye5", add: "active" },
+  { time: 203, target: ".eye5", add: "active" },
   { time: 243, target: ".eye5", remove: "active" },
 
-  { time: 210, target: ".mouth", add: "active" },
+  { time: 204, target: ".mouth", add: "active" },
   { time: 243, target: ".mouth", remove: "active" },
 
-  { time: 212, target: ".mouth2", add: "active" },
+  { time: 205, target: ".mouth2", add: "active" },
   { time: 243, target: ".mouth2", remove: "active" },
 
-  { time: 369, target: ".eyefootage", add: "active" },
+  { time: 399, target: ".eyefootage", add: "active" },
   { time: 408, target: ".eyefootage", remove: "active" },
 
   { time: 7, target: ".rather", add: "active" },
   { time: 21, target: ".rather", remove: "active" },
 
-  { time: 30, target: ".mainpage", add: "blur" },
-  { time: 34, target: ".mainpage", remove: "blur" },
+  { time: 24, target: ".mainpage", add: "blur" },
+  { time: 25, target: ".mainpage", remove: "blur" },
+
+  { time: 114, target: ".mainpage", add: "shift" },
+  { time: 115, target: ".mainpage", remove: "shift" },
+
+  { time: 115, target: ".mainpage", add: "shift2" },
+  { time: 116, target: ".mainpage", remove: "shift2" },
+
+  { time: 116, target: ".mainpage", add: "shift3" },
+  { time: 117, target: ".mainpage", remove: "shift3" },
 
   { time: 21, target: ".p1", add: "highlight" },
   { time: 34, target: ".p1", remove: "highlight" },
@@ -66,7 +86,10 @@ const classChanges = [
   { time: 324, target: ".p10", remove: "highlight" },
 
   { time: 342, target: ".p11", add: "highlight" },
-  { time: 408, target: ".p11", remove: "highlight" },
+  { time: 399, target: ".p11", remove: "highlight" },
+
+  { time: 399, target: ".p12", add: "highlight" },
+  { time: 408, target: ".p12", remove: "highlight" },
 
   { time: 256, target: ".ABLEIST", remove: "smalltext" },  
   { time: 256, target: ".ABLEIST", add: "largetext" }, 
